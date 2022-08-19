a16546d3af0d77e79e9de6657531674c05c7a648
Josh Hobson <joshuahobson1995@gmail.com>
2022-08-19 10:22:23 -0600

    first save sdlc done

---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the process of defining user expectations for the software being built. One is analyzing the requirements of the user and what the user wants the software to do. For example and inventory system would need a search feature for the user to use. A social media site would need the ability to connect with friends"
+const design = "The design phase will lay out the what the software will work. This is a good time to define the technical details of the product. There can be many parts of this process such as, setting the architecture, designing user interface, determining platforms on which the software can run, deciding on a programming language and methods of problem solving, defining methods of communications and security measures. A prototype of the software may also be created at this point in the process to give an idea of what the finished product could look like "
+const implementation = "In short, this is the process of acutally building the application. In this phase the programming code is generated according to all the steps and plans laid out in the steps above. It's writing the code."
+const testingAndIntergration = "Once the program is finished being written it must be tested. This phase involves testing the software for any bugs and making sure the product is running according to plan. Throughout this process, one must find all problems with the software and fix them before pushing the product out to the public or customer. Once the software is free of bugs it can be deployed."
+const maintain = "Now the software is done but not the developers! The software is now being used by customers but it is still up to the software developers to adress issues that customers may have with the application, fix bugs that users find that may have been looked over, or even add new features to the software."
