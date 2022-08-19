239ff3b1a2305296fa269f63f9f215c5ee6ef37c
Josh Hobson <joshuahobson1995@gmail.com>
2022-08-19 12:19:28 -0600

    fifth save

---
+++ password.js
@@ -0,0 +1,16 @@
+let password = "Glucose76!!"
+let lettersLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+  if (password.length >= 10){
+    console.log("good")
+  }
+
+  const chars = password.split('')
+
+for(let i=0; i < password.length; i++) {
+    if(password.split[i] = numbers[0])
+ console.log('good')
+}
