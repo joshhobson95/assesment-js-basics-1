fac9455eb7e0abc9573f4c0655abd3ab7d94c243
Josh Hobson <joshuahobson1995@gmail.com>
2022-08-19 13:32:45 -0600

    Eigth save almost done

--- password.js
+++ password.js
@@ -1,6 +1,6 @@
-let password = '536465431654313465'
+let password = 'Glucose76!!'
 let chars = password.split('')
-// let lettersLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let lettersLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 // let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
@@ -9,6 +9,9 @@ let chars = password.split('')
     console.log("Sufficient Characters")
   }else{
     console.log('Not enough characters')
+  }
+  if (password.length >= 30){
+    console.log("Maximum Characters reached!")
   }
  
 
@@ -35,3 +38,67 @@ if(password[0] <= 9){
 } else{
     console.log('no numbers')
 }
+
+for (var i = 0; i < password.length; i++) {
+ if(password.charAt(i) === lettersLowercase[0])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[1])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[2])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[3])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[4])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[5])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[6])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[7])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[8])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[9])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[10])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[11])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[12])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[13])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[14])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[15])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[16])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[17])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[18])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[19])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[20])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[21])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[22])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[23])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[24])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[25])
+    console.log('one letter')
+else if(password.charAt(i) === lettersLowercase[26])
+    console.log('one letter')
+}
+    
+//Do the same for capital Letters
+//Then try and add {} for only printing out one letter one time, completing the if and not running it through every letter. 
+//also make sure TO TURN IN ON GITHUB AND FRODO LINK THE GITHUB
+
+
+
