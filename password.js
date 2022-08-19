d29550b14c3f76a8165657a888d36e71b1253464
Josh Hobson <joshuahobson1995@gmail.com>
2022-08-19 12:26:10 -0600

    sixth save

--- password.js
+++ password.js
@@ -1,16 +1,18 @@
-let password = "Glucose76!!"
+let password = "Ge76!!"
 let lettersLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 
   if (password.length >= 10){
-    console.log("good")
+    console.log("Sufficient Characters")
+  }else{
+    console.log('Not enough characters')
   }
 
-  const chars = password.split('')
+//   const chars = password.split('')
 
-for(let i=0; i < password.length; i++) {
-    if(password.split[i] = numbers[0])
- console.log('good')
-}
+// for(let i=0; i < password.length; i++) {
+//     if(password[i] = numbers[0])
+//  console.log('great')
+// }
